54190653d72735edde426707fd219d242c7934d5
Vinicius Silva <viniciussilva@tokenlab.com.br>
2020-10-25 00:20:16 -0300

    Adicionado exibição do conteúdo do botão clicado no rótulo de texto

--- app/src/main/java/com/ifsp/pmd/calculadorapdm20202/MainActivity.java
+++ app/src/main/java/com/ifsp/pmd/calculadorapdm20202/MainActivity.java
@@ -1,14 +1,26 @@
 package com.ifsp.pmd.calculadorapdm20202;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private TextView visorTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        visorTv = findViewById(R.id.visorTv);
     }
+
+    public void onClick(View view) {
+        Button button = (Button) view;
+        visorTv.setText(button.getText());
+    }
+
 }
